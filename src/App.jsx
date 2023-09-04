c4023ab423187ae13971239b48b9b4c2a09cfca8
Muhammad Usama <usamajhamat111@gmail.com>
2023-09-03 23:22:38 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import Filter from './Component/Filter.component'
+import PersonForm from './Component/PersonForm.component'
+import Person from './Component/Person.component'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber , setNewNumber] = useState('')
+  const [searchName, setSearchName] = useState('')
+  const addPerson = (event)=>{
+    event.preventDefault()
+    const personObject = {
+      name : newName,
+      number: newNumber
+    }
+    if (persons.some(person => person.name === newName)){
+      alert(`${newName} is already added to Phonbook`)
+    } else {
+      setPersons(persons.concat(personObject))
+      setNewName ('')
+      setNewNumber('')
+    }
+    
+  }
+ 
+ 
+  const filteredPersons = persons.filter(person =>(person.name.toLocaleLowerCase().includes(searchName.toLocaleLowerCase())))
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter searchName={searchName} setSearchName={setSearchName}/>
+      <h3>Add a New</h3>
+      <PersonForm 
+        newName={newName}
+        setNewName={setNewName}
+        newNumber={newNumber}
+        setNewNumber={setNewNumber}
+        addPerson={addPerson}
+      />
+      <h3>Numbers</h3>
+      <Person persons={filteredPersons}/>
+    </div>
+  )
+}
+
+export default App
